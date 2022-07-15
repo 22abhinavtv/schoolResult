19064d6bbd392e6c47eed63cc3c2c102f4db6b63
Abhinav Tv <89732408+22abhinavtv@users.noreply.github.com>
2022-07-15 11:12:41 +0530

    button

--- src/Helpers/util.js
+++ src/Helpers/util.js
@@ -1,8 +1,34 @@
+import { Icon } from "@iconify/react";
 export const Buttons =[{
     btnName : 'Dashboard',
-    btnIcon : <Icon icon="humbleicons:dashboard" width="" rotate={1} hFlip={true} vFlip={true} />
+    btnIcon : <Icon icon="humbleicons:dashboard" width="28" rotate={1} hFlip={true} vFlip={true} />
 },{
-    btnName: 'Courser',
-    btnIcon : 
+    btnName : 'Courses',
+    btnIcon : <Icon icon="healthicons:register-book" width="28"/>,
+    second : true
+},{
+    btnName : 'Students',
+    btnIcon : <Icon icon="fluent:people-team-24-regular" width="28"/>,
+    second : true
+},{
+    btnName : 'Students',
+    btnIcon : <Icon icon="fluent:people-team-24-regular" width="28"/>,
+    second : true
+},{
+    btnName : 'Students',
+    btnIcon : <Icon icon="fluent:people-team-24-regular" width="28"/>,
+    second : true
+},{
+    btnName : 'Students',
+    btnIcon : <Icon icon="fluent:people-team-24-regular" width="28"/>,
+    second : true
+},{
+    btnName : 'Students',
+    btnIcon : <Icon icon="fluent:people-team-24-regular" width="28"/>,
+    second : true
+},{
+    btnName : 'Students',
+    btnIcon : <Icon icon="fluent:people-team-24-regular" width="28"/>,
+    second : true
 }
 ]
